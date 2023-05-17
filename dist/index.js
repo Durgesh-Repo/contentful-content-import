b1d1f687a9f233c166aaf2d4cab6c7989e788419
Durgesh Palanisamy <Durgesh.Palaniswami@mnscorp.net>
2023-05-17 19:16:43 +0100

    Update action

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+const contentfulSpaceId = process.env.CONTENTFUL_SPACE_ID;
+const contentfulAccessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
+
+// Use the retrieved secrets as needed in your code
+console.log('Contentful Space ID:', contentfulSpaceId);
+console.log('Contentful Access Token:', contentfulAccessToken);
