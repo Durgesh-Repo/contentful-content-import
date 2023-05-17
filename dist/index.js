8cb09b1350fc4baea72ccaa3e5065fb449e84ccf
Durgesh Palanisamy <Durgesh.Palaniswami@mnscorp.net>
2023-05-17 19:18:47 +0100

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -4,3 +4,4 @@ const contentfulAccessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
 // Use the retrieved secrets as needed in your code
 console.log('Contentful Space ID:', contentfulSpaceId);
 console.log('Contentful Access Token:', contentfulAccessToken);
+console.log("Index Work");
